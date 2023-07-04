0108a1da2843008407e31c74ab48279d34902480
Bima Setyo <abimanyusrisetyo@gmail.com>
2023-07-04 15:20:11 +0700

    Time: 55 ms (77%), Space: 42 MB (44.22%) - LeetHub

--- 2747-apply-transform-over-each-element-in-array/2747-apply-transform-over-each-element-in-array.js
+++ 2747-apply-transform-over-each-element-in-array/2747-apply-transform-over-each-element-in-array.js
@@ -4,9 +4,13 @@
  * @return {number[]}
  */
 var map = function(arr, fn) {
+    // membuat variabel returnedArray
     returnedArray = [];
+    // mengambil setiap data dalam arr
     for (let i = 0; i < arr.length; i++){
+        // menyimpan ke returnedArray setiap data arr yang telah di operasikan dengan fn
         returnedArray[i] = fn(arr[i], i);
     }
+    // mengembalikan returnedArray
     return returnedArray;
 };
