6592193990a5673257ad8180720dd9a26420c742
Bima Setyo <abimanyusrisetyo@gmail.com>
2023-07-05 09:20:59 +0700

    Time: 51 ms (89.68%), Space: 42.1 MB (26.65%) - LeetHub

---
+++ 2820-return-length-of-arguments-passed/2820-return-length-of-arguments-passed.js
@@ -0,0 +1,11 @@
+/**
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    // menghitung isi data menggunakan length
+    return args.length;
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
