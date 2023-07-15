d82bede7fd871ef35b57123b7af568fbcb96d8e9
Bima Setyo <abimanyusrisetyo@gmail.com>
2023-07-15 20:05:37 +0700

    Time: 76 ms (64.19%), Space: 45.2 MB (68.71%) - LeetHub

---
+++ 1929-concatenation-of-array/1929-concatenation-of-array.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var getConcatenation = function(nums) {
+    // membuat array ans yang berisi hasil concat dari dua nums
+    const ans = nums.concat(nums);
+    return ans;
+};
